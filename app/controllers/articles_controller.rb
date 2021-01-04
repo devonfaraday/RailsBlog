42b873ddd9bf655afd6e9e6147e28cdc357cce01
Christian McMullin <christianmcmullin@gmail.com>
2021-01-03 19:36:10 -0700

    added header, footer, and navigation partial files.  Moved _form to articles folder, added a link to edit an article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,11 +27,19 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    
+
     if @article.update(article_params)
       redirect_to @article
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
   end
 
   private
